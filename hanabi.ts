a2762a013511f8f36f62ac52d97b72d44f4cac83
William Chou <william.chou@bresotec.com>
2020-09-13 12:10:32 -0400

    Add player actions, receive hint and discardCard

--- hanabi.ts
+++ hanabi.ts
@@ -7,13 +7,27 @@ enum CardColor {
 }
 
 class Card {
+  id: number;
   value: number;
   color: CardColor;
 
-  constructor(value: number, color: CardColor) {
+  constructor(id: number, value: number, color: CardColor) {
+    this.id = id;
     this.value = value;
     this.color = color;
   }
+
+  serialize() {
+    return {
+      value: this.value,
+      color: this.color,
+    }
+  }
+}
+
+interface Hint {
+  cardIds: number[];
+  message: string;
 }
 
 class Deck {
@@ -55,7 +69,7 @@ class Deck {
     let i = 0
     for (let color of cardColors) {
       for (let value of cardValues) {
-        cards[i] = new Card(value, color);
+        cards[i] = new Card(i, value, color);
         i += 1
       }
     }
@@ -74,29 +88,50 @@ class Deck {
 }
 
 class Player {
+  id: number;
   hand?: Card[];
-  hints?: string[] | null;
+  hints: Hint[];
+
+  constructor(id: number){
+    this.id = id
+    this.hints = []
+  }
 
   getInitialHand(cards: Card[]) {
     this.hand = cards;
   }
 
-  receiveHint(hint: string) {
-    this.hints?.push(hint)
+  receiveHint(hint: Hint) {
+    this.hints.push(hint)
+    console.log(this.hints)
+  }
+
+  discardCard(cardId: number) {
+    this.hand = this.hand.filter(card => card !== cardId)
+  }
+
+  serialize() {
+    return {
+      id: this.id,
+      hand: this.hand?.map(card => card.serialize()),
+      hints: this.hints,
+    }
   }
 }
 
-class Hanabi {
+export class Hanabi {
   deck: Deck;
   numPlayers: number;
   players: Player[];
+  blueTokens = 8;
+  redTokens = 3;
 
   constructor(numPlayers: number) {
     this.deck = new Deck();
     this.numPlayers = numPlayers;
     this.players = Array(this.numPlayers)
     for (let i = 0; i < this.numPlayers; i++) {
-      this.players[i] = new Player()
+      this.players[i] = new Player(i)
     }
 
     this.dealInitialHands()
@@ -105,7 +140,7 @@ class Hanabi {
   dealInitialHands() {
     let playerIndex = 0;
     const hands: Card[][] = []
-    while(this.deck.length >= 50 - this.numPlayers * 5) {
+    while(this.deck.length > 50 - this.numPlayers * 5) {
       if (!hands[playerIndex]) {
         hands[playerIndex] = []
       }
@@ -117,10 +152,34 @@ class Hanabi {
       this.players[i].getInitialHand(hands[i])
     }
   }
+
+  giveHint(playerId: number, hint: Hint) {
+    if (this.blueTokens === 0) {
+      throw new Error('No blue tokens left, must discard a card to get more.')
+    }
+    this.blueTokens -= 1
+    this.players[playerId].receiveHint(hint)
+  }
+
+  discardCard(playerId: number, cardId: number) {
+    this.players[playerId].discardCard(cardId)
+    this.blueTokens += 1
+    this.players[playerId].hand?.push(this.deck.drawCard())
+  }
+
+  /**
+   * Return game state to the client, with the player id as a paramater.
+   * The player should not be able to see their own cards.
+   * 
+   * @param playerId id of the player viewing the game.
+   */
+  serialize(playerId: number) {
+    return this.players.filter(player => player.id !== playerId).map(player => player.serialize())
+  }
 }
 
-const hanabi = new Hanabi(4)
-console.log(hanabi.deck.cards[0])
-console.log(hanabi.deck.cards[45])
-console.log(hanabi.deck.cards[20])
-console.log(hanabi.players[0] && hanabi.players[1].hand)
+// const hanabi = new Hanabi(4)
+// console.log(hanabi.deck.cards[0])
+// console.log(hanabi.deck.cards[45])
+// console.log(hanabi.deck.cards[20])
+// console.log(hanabi.players[0] && hanabi.players[1].hand)
